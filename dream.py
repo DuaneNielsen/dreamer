dac7de3d3a862794769af6ea2b56025c6d82e02d
niedu02 <duane.nielsen.rocks@gmail.com>
2018-12-10 12:35:51 -0800

    Visualise dreamer

--- dream.py
+++ dream.py
@@ -3,24 +3,42 @@ from mentalitystorm.storage import Storeable
 from mentalitystorm.config import config
 from mentalitystorm.data_containers import ActionEmbedding
 import gym
+from mentalitystorm.observe import UniImageViewer
+from time import sleep
 
-gym_environment = 'SpaceInvaders-v4'
+def main(model, controller, gym_environment, device, decoder=None):
 
-device = config.device()
-model = Storeable.load(r'C:\data\runs\687\mdnrnn-i_size-6-z_size-16-hidden_size-256-num_layers-1-n_gaussians-5_2.md').to(device)
-controller = torch.load(r'.\modelzoo\best_model68').to(device)
+    if decoder is not None:
+        decoder = decoder.to(device)
+        decoder.register_forward_hook(UniImageViewer('decoded_latent_space', (320, 480)).view_output)
 
-env = gym.make(gym_environment)
-start_action = ActionEmbedding(env=env).start_tensor().to(device)
-context = (torch.zeros(1, 1, 256).to(device), torch.zeros(1, 1, 256).to(device))
-
-pi, mu, sigma, context = model.step(start_action.unsqueeze(0).unsqueeze(0), context)
-observation = model.sample(pi, mu, sigma)
-
-for frame in range(1000):
-    action = controller(observation.double())
-    _, index = action.max(2)
-    action = torch.zeros_like(action)
-    action[0, 0, index] = 1.0
-    pi, mu, sigma, context = model.step(action.float(), context)
+    env = gym.make(gym_environment)
+    start_action = ActionEmbedding(env=env).start_tensor().to(device)
+    context = (torch.zeros(1, 1, 256).to(device), torch.zeros(1, 1, 256).to(device))
+    pi, mu, sigma, context = model.step(start_action.unsqueeze(0).unsqueeze(0), context)
     observation = model.sample(pi, mu, sigma)
+
+    for frame in range(1000):
+        if decoder is not None:
+            decoder(observation.unsqueeze(0).transpose(3,1))
+        action = controller(observation.double())
+        _, index = action.max(2)
+        action = torch.zeros_like(action)
+        action[0, 0, index] = 1.0
+        pi, mu, sigma, context = model.step(action.float(), context)
+        observation = model.sample(pi, mu, sigma)
+        sleep(0.05)
+
+
+if __name__ == '__main__':
+
+    gym_environment = 'SpaceInvaders-v4'
+    device = config.device()
+    model = Storeable.load(
+        r'C:\data\runs\687\mdnrnn-i_size-6-z_size-16-hidden_size-256-num_layers-1-n_gaussians-5_2.md').to(device)
+    controller = torch.load(r'.\modelzoo\best_model68').to(device)
+
+    visuals = Storeable.load('.\modelzoo\GM53H301W5YS38XH')
+    decoder = visuals.decoder
+
+    main(model, controller, gym_environment, device, decoder)
