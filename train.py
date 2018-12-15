80cfe0f838ca91d26b39fcd19b8221074547f8c6
niedu02 <duane.nielsen.rocks@gmail.com>
2018-12-14 23:54:48 -0800

    Time shifted the target Input is now action + z at t = 0 Target is z at t + 1

--- train.py
+++ train.py
@@ -33,13 +33,14 @@ def timeline(batch_size, timesteps, z_size, device):
 if __name__ == '__main__':
 
     batch_size = 128
+    i_size = 16 + 6
     z_size = 16
     train_model = True
     config.increment('run_id')
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    dataset = ActionEncoderDataset(config.datapath('SpaceInvaders-v4/rl_raw_v2'))
+    dataset = ActionEncoderDataset(config.datapath('SpaceInvaders-v4/rl_raw_v2'), load_observation=False, load_screen=False)
 
     dev = data_utils.Subset(dataset, range(dataset.count * 2 // 10))
     train = data_utils.Subset(dataset, range(0, dataset.count * 9//10))
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     ground_truth_decoder = Storeable.load('C:\data\models\GM53H301W5YS38XH').to(device)
     ground_truth_decoder.decoder.register_forward_hook(UniImageViewer('ground_truth').view_input)
 
-    model = MDNRNN(i_size=6, z_size=z_size, hidden_size=32, num_layers=5, n_gaussians=3).to(device)
+    model = MDNRNN(i_size=i_size, z_size=z_size, hidden_size=32, num_layers=3, n_gaussians=3).to(device)
 
     #model = Storeable.load(r'C:\data\runs\687\mdnrnn-i_size-6-z_size-16-hidden_size-256-num_layers-1-n_gaussians-5_10.md').to(device)
 
@@ -65,6 +66,15 @@ if __name__ == '__main__':
     global_step = 0
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    def temporal_shift_and_diff(action, latent):
+        action_latent = [torch.cat(act_z, dim=1)[:-1] for act_z in zip(action, latent)]
+        z_plus_1_delta = []
+        for z in latent:
+            z_plus_1 = z.clone()[1:]
+            z_plus_1_delta.append(z_plus_1 - z[:-1])
+        return action_latent, z_plus_1_delta
+
 
     for epoch in range(1, 21):
         train = tqdm(train, desc=f'train {epoch}')
@@ -74,10 +84,12 @@ if __name__ == '__main__':
             #seq_length = max_seq_length(observation_minibatch)
             #tl = timeline(batch_size, seq_length, z_size, device)
 
-            pi, mu, sigma, _ = model(action, device)
-            padded_latent = rnn_utils.pad_sequence(latent, batch_first=True).squeeze().to(device)
-            padded_latent = padded_latent.clamp(1e-10, 1e10)  # clamp to avoid exploding gradients
-            loss = model.loss_fn(padded_latent, pi, mu, sigma)
+            action_latent = [torch.cat(act_z, dim=1)[:-1] for act_z in zip(action, latent)]
+            latent_t_plus_1 = [t.clone()[1:] for t in latent]
+            pi, mu, sigma, _ = model(action_latent, device)
+            padded_latent_t_plus_1 = rnn_utils.pad_sequence(latent_t_plus_1, batch_first=True).squeeze().to(device)
+            #padded_latent = padded_latent.clamp(1e-1, 1e1)
+            loss = model.loss_fn(padded_latent_t_plus_1, pi, mu, sigma)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -90,9 +102,11 @@ if __name__ == '__main__':
         test = tqdm(test, desc=f'test {epoch}')
         losses = []
         for screen, observation, action, reward, done, latent in test:
-            pi, mu, sigma, _ = model(action, device)
-            padded_latent = rnn_utils.pad_sequence(latent, batch_first=True).squeeze().to(device)
-            loss = model.loss_fn(padded_latent, pi, mu, sigma)
+            action_latent = [torch.cat(act_z, dim=1)[:-1] for act_z in zip(action, latent)]
+            latent_t_plus_1 = [t.clone()[1:] for t in latent]
+            pi, mu, sigma, _ = model(action_latent, device)
+            padded_latent_t_plus_1 = rnn_utils.pad_sequence(latent_t_plus_1, batch_first=True).squeeze().to(device)
+            loss = model.loss_fn(padded_latent_t_plus_1, pi, mu, sigma)
             losses.append(loss.item())
             test.set_description(f'test epoch: {epoch} loss :{mean(losses)}')
             tb.add_scalar('test/loss', loss.item(), global_step)
